319643442bf5dc8dfe49237495c7fa20852a5b72
Hitesh Bhagwat <155135951+Hiteshdevss@users.noreply.github.com>
2024-08-14 20:06:52 +0530

    Add files via upload

---
+++ Pages/Channel_Partner.php
@@ -0,0 +1,220 @@
+<!doctype html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <title>SQFTINFRA | Channel Partner</title>
+
+</head>
+
+<body>
+  <!-- Header -->
+  <?php include './Components/Navbar.php';  ?>
+
+  <!-- Fix Banner Image  -->
+  <div class="p-2 md:p-0 mr-0 md:mr-36 ml-0 md:ml-36 mt-0 md:mt-10">
+    <!-- visible on desktop only -->
+    <img src="../Assets/Images/Page_Banner/Desktop/C&P-bg.png" alt="" class="mt-2 rounded-lg hidden md:block">
+
+    <!-- visible on mobile only -->
+    <img src="../Assets/Images/Page_Banner/Mobile/m-C&P-bg.png" alt="" class="mt-2 rounded-lg md:hidden">
+  </div>
+
+
+  <section class="text-gray-600 body-font mr-0 md:mr-36 ml-0 md:ml-36">
+    <h2 class="text-center mt-20 -mb-0 md:-mb-10 text-5xl text-gray-900 font-extrabold">Our <span class='bg-gradient-to-r from-purple-600 to-pink-600 hover:bg-gradient-to-r hover:from-purple-900 hover:to-pink-900 text-white text-transparent bg-clip-text'>Community</span></h2>
+    <div class="container px-5 py-10 md:py-24 mx-auto">
+      <div class="flex flex-wrap justify-center md:justify-between -m-4">
+
+        <div class="p-4 w-full sm:w-1/2 md:w-1/4 lg:w-1/4">
+          <div class="h-full flex flex-col items-center text-center">
+            <img alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="../Assets/Images/channel_partners/cp1.jpeg">
+            <div class="w-full">
+              <h2 class="title-font font-medium text-lg text-gray-900">Rajkumar Warget</h2>
+              <h3 class="text-gray-500 mb-3">SQFT INFRA Sales Director</h3>
+              <span class="inline-flex">
+
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="p-4 w-full sm:w-1/2 md:w-1/4 lg:w-1/4">
+          <div class="h-full flex flex-col items-center text-center">
+            <img alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="../Assets/Images/channel_partners/cp2.jpeg">
+            <div class="w-full">
+              <h2 class="title-font font-medium text-lg text-gray-900">Vikrant Thulkar</h2>
+              <h3 class="text-gray-500 mb-3">SQFT INFRA Director</h3>
+              <span class="inline-flex">
+
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div class="p-4 w-full sm:w-1/2 md:w-1/4 lg:w-1/4">
+          <div class="h-full flex flex-col items-center text-center">
+            <img alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="../Assets/Images/channel_partners/cp7.jpeg">
+            <div class="w-full">
+              <h2 class="title-font font-medium text-lg text-gray-900">Anilkumar Hadke</h2>
+              <h3 class="text-gray-500 mb-3">SQFT INFRA Director</h3>
+              <span class="inline-flex">
+
+              </span>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="p-4 w-full sm:w-1/2 md:w-1/4 lg:w-1/4">
+          <div class="h-full flex flex-col items-center text-center">
+            <img alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="../Assets/Images/channel_partners/5.png">
+            <div class="w-full">
+              <h2 class="title-font font-medium text-lg text-gray-900">Dyneshwar Khuje</h2>
+              <h3 class="text-gray-500 mb-3">SQFT INFRA Director</h3>
+              <span class="inline-flex">
+
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="p-4 w-full sm:w-1/2 md:w-1/4 lg:w-1/4">
+          <div class="h-full flex flex-col items-center text-center">
+            <img alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="../Assets/Images/channel_partners/7.png">
+            <div class="w-full">
+              <h2 class="title-font font-medium text-lg text-gray-900">Gajanan Urkude</h2>
+              <h3 class="text-gray-500 mb-3">GEETAI GROUP Director</h3>
+              <span class="inline-flex">
+
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="p-4 w-full sm:w-1/2 md:w-1/4 lg:w-1/4">
+          <div class="h-full flex flex-col items-center text-center">
+            <img alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="../Assets/Images/channel_partners/1.png">
+            <div class="w-full">
+              <h2 class="title-font font-medium text-lg text-gray-900">Vinay Sonule</h2>
+              <h3 class="text-gray-500 mb-3">Amaravti Area Director</h3>
+              <span class="inline-flex">
+
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="p-4 w-full sm:w-1/2 md:w-1/4 lg:w-1/4">
+          <div class="h-full flex flex-col items-center text-center">
+            <img alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="../Assets/Images/channel_partners/2.png">
+            <div class="w-full">
+              <h2 class="title-font font-medium text-lg text-gray-900">Ashish Raut</h2>
+              <h3 class="text-gray-500 mb-3">GEETAI GROUP</h3>
+              <span class="inline-flex">
+
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div class="p-4 w-full sm:w-1/2 md:w-1/4 lg:w-1/4">
+          <div class="h-full flex flex-col items-center text-center">
+            <img alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="../Assets/Images/channel_partners/3.png">
+            <div class="w-full">
+              <h2 class="title-font font-medium text-lg text-gray-900">Milind Lonpande</h2>
+              <h3 class="text-gray-500 mb-3">Amaravti Area Director</h3>
+              <span class="inline-flex">
+
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div class="p-4 w-full sm:w-1/2 md:w-1/4 lg:w-1/4">
+          <div class="h-full flex flex-col items-center text-center">
+            <img alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="../Assets/Images/channel_partners/cp3.jpeg">
+            <div class="w-full">
+              <h2 class="title-font font-medium text-lg text-gray-900">Swaraj Sontakke</h2>
+              <h3 class="text-gray-500 mb-3">Business Partners</h3>
+              <span class="inline-flex">
+
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="p-4 w-full sm:w-1/2 md:w-1/4 lg:w-1/4">
+          <div class="h-full flex flex-col items-center text-center">
+            <img alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="../Assets/Images/channel_partners/6.png">
+            <div class="w-full">
+              <h2 class="title-font font-medium text-lg text-gray-900">Rajesh Patil</h2>
+              <h3 class="text-gray-500 mb-3">Business Partners</h3>
+              <span class="inline-flex">
+
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="p-4 w-full sm:w-1/2 md:w-1/4 lg:w-1/4">
+          <div class="h-full flex flex-col items-center text-center">
+            <img alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="../Assets/Images/channel_partners/cp4.jpeg">
+            <div class="w-full">
+              <h2 class="title-font font-medium text-lg text-gray-900">Nagsen Khobragade</h2>
+              <h3 class="text-gray-500 mb-3">Business Partners</h3>
+              <span class="inline-flex">
+
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="p-4 w-full sm:w-1/2 md:w-1/4 lg:w-1/4">
+          <div class="h-full flex flex-col items-center text-center">
+            <img alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="../Assets/Images/channel_partners/cp5.jpeg">
+            <div class="w-full">
+              <h2 class="title-font font-medium text-lg text-gray-900">Shivraj Rangari</h2>
+              <h3 class="text-gray-500 mb-3">Business Partners</h3>
+              <span class="inline-flex">
+
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="p-4 w-full sm:w-1/2 md:w-1/4 lg:w-1/4">
+          <div class="h-full flex flex-col items-center text-center">
+            <img alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="../Assets/Images/channel_partners/cp6.jpeg">
+            <div class="w-full">
+              <h2 class="title-font font-medium text-lg text-gray-900">Kishore Chunarkar</h2>
+              <h3 class="text-gray-500 mb-3">Business Partners</h3>
+              <span class="inline-flex">
+
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <!-- <div class="p-4 w-full sm:w-1/2 md:w-1/4 lg:w-1/4">
+          <div class="h-full flex flex-col items-center text-center">
+            <img alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="https://dummyimage.com/203x203">
+            <div class="w-full">
+              <h2 class="title-font font-medium text-lg text-gray-900">Henry Letham</h2>
+              <h3 class="text-gray-500 mb-3">UI Developer</h3>
+              <span class="inline-flex">
+
+              </span>
+            </div>
+          </div>
+        </div> -->
+      </div>
+    </div>
+  </section>
+
+
+
+
+
+  <!-- footer -->
+  <div class="h-10"></div>
+  <?php include './Components/Footer.php';  ?>
+  <?php include './Components/Whatsapp.php' ?>
+
+</body>
+
+</html>
